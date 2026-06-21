a522163fe3c792a14e3df3932fcc8f089e2ef0d7
narendermanomay-svg <narender.manomay@gmail.com>
2026-06-22 00:16:54 +0530

    Adjust logo height in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -86,7 +86,7 @@ export default function Navbar({ activeSection, onNavigate }: NavbarProps) {
           <img
            src={logo}
            alt="BharatGreenCarbon"
-           className="h-28 w-auto"
+           className="h-26 w-auto"
          />
        </button>
 
