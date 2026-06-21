f0e8a517743eb8cbf02a22136e57692fd4da5f01
narendermanomay-svg <narender.manomay@gmail.com>
2026-06-21 12:17:17 +0530

    Refactor sitemap plugin import in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-import { VitePluginSitemap } from 'vite-plugin-sitemap';
+import sitemap from 'vite-plugin-sitemap';
 export default defineConfig(() => {
   return {
     plugins: [
     react(),
       tailwindcss(),
-        VitePluginSitemap({
+        sitemap({
       hostname: 'https://bharatgreencarbon.com'
     }),
     ],
